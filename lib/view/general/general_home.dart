c01ae8b5174ee552c00b76315c9ed5035bf93871
Ktento <kento07210525mo@outlook.jp>
2025-10-10 16:13:35 +0900

    一般モードのナビゲーションを作成

--- lib/view/general/general_home.dart
+++ lib/view/general/general_home.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'general_info.dart';
+import 'general_map.dart';
+import 'general_post.dart';
 
-class Jeneral_HomePage extends StatelessWidget {
-  const Jeneral_HomePage({super.key});
+class General_HomePage extends StatefulWidget {
+  const General_HomePage({super.key});
+
+  @override
+  State<General_HomePage> createState() => _General_HomePageState();
+}
+
+class _General_HomePageState extends State<General_HomePage> {
+  int _selectedIndex = 0; // 選択中のタブのインデックス
+  // タブごとの画面リスト
+  static const List<Widget> _pages = <Widget>[
+    GeneralMap(), // マップ画面
+    GeneralPost(), // 投稿画面
+    GeneralInfo(), // 被害情報画面
+  ];
+  // タブを押したときに呼ばれる関数
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('ホーム')),
-        body: const Center(
-          child: Text("一般モード"),
-        ));
+      appBar: AppBar(title: const Text('ホーム')),
+      body: _pages[_selectedIndex], // 選択中のタブに応じて画面を切り替える
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'マップ'),
+          BottomNavigationBarItem(icon: Icon(Icons.upload), label: '投稿'),
+          BottomNavigationBarItem(icon: Icon(Icons.warning), label: '被害情報'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
