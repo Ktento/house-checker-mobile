df87fc3f08b5d50afb0e6551148312a684170f6a
Ktento <kento07210525mo@outlook.jp>
2025-10-17 12:11:48 +0900

    現在位置にスムーズに戻るボタンを追加

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -1,9 +1,13 @@
 import 'package:latlong2/latlong.dart';
 import '../models/map_model.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:flutter/material.dart';
 
 class MapControllerMVC {
   final MapModel _model = MapModel();
+  final AnimatedMapController animatedMapController;
 
+  MapControllerMVC(this.animatedMapController);
   MapModel get model => _model;
 
   // 中心位置を変更
@@ -14,5 +18,15 @@ class MapControllerMVC {
   // マーカーを追加
   void addMarker(LatLng position) {
     _model.markers.add(position);
+  }
+
+  // アニメーションで指定位置に移動
+  void moveToLocation(LatLng latlng) {
+    animatedMapController.animateTo(
+      dest: latlng,
+      zoom: 19,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOut,
+    );
   }
 }
