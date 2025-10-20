1c78c648fbc4e85558c1aeebe28ad888c110b8aa
Ktento <kento07210525mo@outlook.jp>
2025-10-20 15:49:04 +0900

    InvestigatorPostControllerクラスを新規作成し、複数のTextEditingControllerを追加

---
+++ lib/controllers/investigator_post_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InvestigatorPostController {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController numberController = TextEditingController();
+  final TextEditingController prefectureController = TextEditingController();
+  final TextEditingController investigatorNumberController =
+      TextEditingController();
+  final TextEditingController countController = TextEditingController();
+}
