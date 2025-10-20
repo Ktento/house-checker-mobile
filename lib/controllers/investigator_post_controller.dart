f4ca3cc93172ea8d760ef9d8d322cce1eb4ee2c8
Ktento <kento07210525mo@outlook.jp>
2025-10-20 16:33:26 +0900

    InvestigatorPostControllerのcreateInvestigationUnitメソッドを追加し、WoodenResearchUnitでの調査単位生成を簡素化

--- lib/controllers/investigator_post_controller.dart
+++ lib/controllers/investigator_post_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/investigator_post_model.dart';
 
 class InvestigatorPostController {
   final TextEditingController nameController = TextEditingController();
@@ -7,4 +8,16 @@ class InvestigatorPostController {
   final TextEditingController investigatorNumberController =
       TextEditingController();
   final TextEditingController countController = TextEditingController();
+
+  /// 入力内容から InvestigationUnit モデルを生成
+  InvestigationUnit createInvestigationUnit(DateTime now) {
+    return InvestigationUnit(
+      date: now, // 現在日時を設定（手動入力でも可）
+      investigator: [nameController.text],
+      number: numberController.text,
+      prefecture: [prefectureController.text],
+      surveyCount: int.tryParse(countController.text) ?? 1, // 数字で変換
+      investigatorNumber: [investigatorNumberController.text],
+    );
+  }
 }
