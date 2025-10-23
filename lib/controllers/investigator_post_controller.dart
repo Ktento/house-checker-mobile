530e688235ce5e05fdc6aac79fba8b9391009f04
Ktento <kento07210525mo@outlook.jp>
2025-10-23 10:53:25 +0900

    建築物概要用のコントローラー、モデル生成関数を追加

--- lib/controllers/investigator_post_controller.dart
+++ lib/controllers/investigator_post_controller.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import '../models/investigator_post_model.dart';
 
 class InvestigatorPostController {
+  //調査単位用のコントローラー
   final TextEditingController nameController = TextEditingController();
   final TextEditingController numberController = TextEditingController();
   final TextEditingController prefectureController = TextEditingController();
   final TextEditingController investigatorNumberController =
       TextEditingController();
   final TextEditingController countController = TextEditingController();
+
+  //建築物概要用のコントローラー
+  final TextEditingController buildingNameController = TextEditingController();
+  final TextEditingController buildingNumberController =
+      TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController mapNumberController = TextEditingController();
+  final TextEditingController buildingUseController = TextEditingController();
+  final TextEditingController structureController = TextEditingController();
+  final TextEditingController floorsController = TextEditingController();
+  final TextEditingController scaleController = TextEditingController();
 
   /// 入力内容から InvestigationUnit モデルを生成
   InvestigationUnit createInvestigationUnit(DateTime now) {
@@ -18,6 +30,19 @@ class InvestigatorPostController {
       prefecture: [prefectureController.text],
       surveyCount: int.tryParse(countController.text) ?? 1, // 数字で変換
       investigatorNumber: [investigatorNumberController.text],
+    );
+  }
+
+  BuildingOverview createBuildingOverview() {
+    return BuildingOverview(
+      buildingName: buildingNameController.text,
+      buildingNumber: buildingNumberController.text,
+      address: addressController.text,
+      mapNumber: mapNumberController.text,
+      buildingUse: buildingUseController.text,
+      structure: structureController.text,
+      floors: int.tryParse(floorsController.text) ?? 1,
+      scale: scaleController.text,
     );
   }
 }
