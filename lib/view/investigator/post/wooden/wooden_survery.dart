300ce8018e4700de02de581061d025b58b53456f
Ktento <kento07210525mo@outlook.jp>
2025-10-28 15:35:02 +0900

    CupertinoDropdownを使用して外観調査点数などの選択肢を追加

--- lib/view/investigator/post/wooden/wooden_survery.dart
+++ lib/view/investigator/post/wooden/wooden_survery.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:house_check_mobile/view/investigator/post/wooden/wooden_check.dart';
 import '../../../../models/investigator_post_model.dart';
 import '../../../../controllers/investigator_post_controller.dart';
-import 'package:house_check_mobile/utils/dialog_helper.dart';
+import 'package:house_check_mobile/utils/widgets/dialog.dart';
+import '../../../../utils/db_service.dart';
+import '../../../../utils/components/choose_picker.dart';
 
 class WoodenSurvey extends StatefulWidget {
   const WoodenSurvey(
@@ -47,10 +49,14 @@ class _WoodenSurveyState extends State<WoodenSurvey> {
       //調査内容のモデルを作成
       InvestigationContent investigationContent =
           controller.createInvestigationContent();
-
+      //すべてをまとめたモデルを作成
+      InvestigationRecord record = controller.createInvestigationRecord(
+          widget.unit, widget.buildingOverview, investigationContent);
+      sendRecord(record);
       Navigator.push(
         context,
-        CupertinoPageRoute(builder: (context) => const WoodenCheck()),
+        CupertinoPageRoute(
+            builder: (context) => const CupertinoDropdownExample()),
       );
     }
   }
@@ -100,52 +106,278 @@ class _WoodenSurveyState extends State<WoodenSurvey> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildCupertinoTextField(
-                      label: '外観調査点数',
-                      controller: controller.exteriorInspectionScoreController),
+                  const Text(
+                    '外観調査点数',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      '1.建物全体又は一部の崩壊・落階',
+                      '2.基礎の著しい破壊、上部構造との著しいずれ',
+                      '3.建物全体又は一部の著しい傾斜',
+                      '4.その他',
+                      '5.なし'
+                    ],
+                    initialValue: '5.なし',
+                    onChanged: (value) {
+                      controller.exteriorInspectionScoreController.text = value;
+                    },
+                  ),
                   _buildCupertinoTextField(
                       label: '外観調査備考',
                       controller:
                           controller.exteriorInspectionRemarksController),
-                  _buildCupertinoTextField(
-                      label: '隣接建築物・周辺地盤の危険',
-                      controller: controller.adjacentBuildingRiskController),
-                  _buildCupertinoTextField(
-                      label: '構造躯体の不同沈下',
-                      controller: controller.unevenSettlementController),
-                  _buildCupertinoTextField(
-                    label: '基礎の被害',
-                    controller: controller.foundationDamageController,
+                  const Text(
+                    '隣接建築物・周辺地盤の危険',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
                   ),
-                  _buildCupertinoTextField(
-                      label: ' 建築物の1階の傾斜',
-                      controller: controller.firstFloorTiltController),
-                  _buildCupertinoTextField(
-                      label: '壁の被害',
-                      controller: controller.wallDamageController),
-                  _buildCupertinoTextField(
-                      label: '腐食・蟻害の有無',
-                      controller: controller.corrosionOrTermiteController),
-                  _buildCupertinoTextField(
-                      label: '瓦',
-                      controller: controller.roofOrSignboardRiskController),
-                  _buildCupertinoTextField(
-                      label: '窓枠・窓ガラス',
-                      controller: controller.windowFrameController),
-                  _buildCupertinoTextField(
-                      label: '外装材（湿式）',
-                      controller: controller.exteriorWetController),
-                  _buildCupertinoTextField(
-                      label: '外装材（乾式）',
-                      controller: controller.exteriorDryController),
-                  _buildCupertinoTextField(
-                      label: '看板・機器類',
-                      controller: controller.signageAndEquipmentController),
-                  _buildCupertinoTextField(
-                      label: '屋外階段',
-                      controller: controller.outdoorStairsController),
-                  _buildCupertinoTextField(
-                      label: 'その他', controller: controller.othersController),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.危険無し',
+                      'B.不明確',
+                      'C.危険あり',
+                    ],
+                    initialValue: 'A.危険無し',
+                    onChanged: (value) {
+                      controller.adjacentBuildingRiskController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '構造躯体の不同沈下',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.無し又は軽微',
+                      'B.著しい床、屋根の落ち込み、浮き上がり',
+                      'C.小屋組みの破壊、床全体の沈下',
+                    ],
+                    initialValue: 'A.無し又は軽微',
+                    onChanged: (value) {
+                      controller.unevenSettlementController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '基礎の被害',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.無被害',
+                      'B.部分的',
+                      'C.著しい(被害あり)',
+                    ],
+                    initialValue: 'A.無被害',
+                    onChanged: (value) {
+                      controller.foundationDamageController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '建築物の1階の傾斜',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.1/60以下',
+                      'B.1/60～1/20',
+                      'C.1/20超',
+                    ],
+                    initialValue: 'A.1/60以下',
+                    onChanged: (value) {
+                      controller.firstFloorTiltController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '壁の被害',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.軽微なひび割れ',
+                      'B.大きな亀裂、剥離',
+                      'C.落下の危険有り',
+                    ],
+                    initialValue: 'A.軽微なひび割れ',
+                    onChanged: (value) {
+                      controller.wallDamageController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '腐食・蟻害の有無',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.ほとんど無し',
+                      'B.一部の断面欠損',
+                      'C.著しい断面欠損',
+                    ],
+                    initialValue: 'A.ほとんど無し',
+                    onChanged: (value) {
+                      controller.corrosionOrTermiteController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '瓦',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.ほとんど無被害',
+                      'B.著しいずれ',
+                      'C.全面的にずれ、破損',
+                    ],
+                    initialValue: 'A.ほとんど無被害',
+                    onChanged: (value) {
+                      controller.roofOrSignboardRiskController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '窓枠・窓ガラス',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.ほとんど無被害',
+                      'B.歪み、ひび割れ',
+                      'C.落下の危険有',
+                    ],
+                    initialValue: 'A.ほとんど無被害',
+                    onChanged: (value) {
+                      controller.windowFrameController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '外装材　湿式の場合',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.ほとんど無被害',
+                      'B.部分的なひび割れ、隙間',
+                      'C.顕著なひび割れ、剥離',
+                    ],
+                    initialValue: 'A.ほとんど無被害',
+                    onChanged: (value) {
+                      controller.exteriorWetController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '外装材　乾式の場合',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.目地の亀裂程度',
+                      'B.板に隙間がみられる',
+                      'C.顕著な目地ずれ、板破損',
+                    ],
+                    initialValue: 'A.目地の亀裂程度',
+                    onChanged: (value) {
+                      controller.exteriorDryController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '看板・機器類',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.傾斜無し',
+                      'B.わずかな傾斜',
+                      'C.落下の危険有り',
+                    ],
+                    initialValue: 'A.傾斜無し',
+                    onChanged: (value) {
+                      controller.signageAndEquipmentController.text = value;
+                    },
+                  ),
+                  const Text(
+                    '屋外階段',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.傾斜なし',
+                      'B.わずかな傾斜',
+                      'C.明瞭な傾斜',
+                    ],
+                    initialValue: 'A.傾斜なし',
+                    onChanged: (value) {
+                      controller.outdoorStairsController.text = value;
+                    },
+                  ),
+                  const Text(
+                    'その他',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CupertinoDropdown(
+                    options: [
+                      'A.安全',
+                      'B.要注意',
+                      'C.危険',
+                    ],
+                    initialValue: 'A.安全',
+                    onChanged: (value) {
+                      controller.othersController.text = value;
+                    },
+                  ),
                   _buildCupertinoTextField(
                       label: 'その他の内容',
                       controller: controller.otherRemarksController),
