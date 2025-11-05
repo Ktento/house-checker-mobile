833361588022c8876ac8e0d4c61f05a09417ca91
Ktento <kento07210525mo@outlook.jp>
2025-11-05 10:06:48 +0900

    ファイル位置を修正

--- lib/utils/services/db_service.dart
+++ lib/utils/services/db_service.dart
@@ -1,11 +1,11 @@
-import '../models/investigator_post_model.dart';
+import '../../models/investigator_post_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void sendRecord(InvestigationRecord record) async {
   try {
     final url = Uri.parse(
-        'https://script.google.com/macros/s/AKfycbw9pz9QFY0PqUXGvYuEzISLJ_tyvuXrhKgk2F6NcVXA5Pqd_cWUZnQ8ja6X54-P8q7u/exec');
+        'https://script.google.com/macros/s/AKfycbxm62dd1yIWL6WWFN4Hr5J1hzjME79WUFYle5AY91kfsfrlmNYQimCaCWdlXLV_ocb3/exec');
     // 送信前に JSON を確認
     final jsonBody = jsonEncode(record.toJson());
     print("送信する JSON:\n$jsonBody");
