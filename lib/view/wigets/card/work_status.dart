b5c7f5fea59749987e803379b60db472cb20da0c
Ktento <kento07210525mo@outlook.jp>
2025-11-06 17:53:39 +0900

    判定士の作業状況を表示する機能を追加

---
+++ lib/view/wigets/card/work_status.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/cupertino.dart';
+
+class WorkStatus extends StatefulWidget {
+  const WorkStatus({super.key});
+
+  @override
+  State<WorkStatus> createState() => _WorkStatusState();
+}
+
+class _WorkStatusState extends State<WorkStatus> {
+  @override
+  Widget build(BuildContext context) {
+    final inspectors = [
+      {'name': '田中判定士', 'progress': '80%'},
+      {'name': '松本判定士', 'progress': '11.9%'},
+      {'name': '佐藤判定士', 'progress': '42%'},
+      {'name': '高橋判定士', 'progress': '67%'},
+      {'name': '中村判定士', 'progress': '25%'},
+      {'name': '伊藤判定士', 'progress': '93%'},
+    ];
+
+    return Container(
+      width: 252,
+      height: 100,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 112, 112, 219), // 背景色
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          // 黒い枠線を追加
+          color: CupertinoColors.black,
+          width: 1,
+        ), // 角を丸く
+        boxShadow: [
+          BoxShadow(
+            color: CupertinoColors.systemGrey3.withOpacity(0.5),
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 12, top: 3),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(children: [
+              Text(
+                '判定士作業状況',
+                style: TextStyle(
+                  color: CupertinoColors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(CupertinoIcons.graph_square,
+                  color: CupertinoColors.white, size: 20),
+            ]),
+            SizedBox(height: 10),
+            Expanded(
+              child: CupertinoScrollbar(
+                thickness: 6.0,
+                thicknessWhileDragging: 10.0,
+                radius: const Radius.circular(34.0),
+                radiusWhileDragging: Radius.zero,
+                thumbVisibility: true,
+                child: ListView.builder(
+                    itemCount: inspectors.length,
+                    itemBuilder: (context, index) {
+                      final inspector = inspectors[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 8),
+                        child: Row(
+                          children: [
+                            Text(
+                              inspector['name']!,
+                              style: const TextStyle(
+                                color: CupertinoColors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              inspector['progress']!,
+                              style: const TextStyle(
+                                color: CupertinoColors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
