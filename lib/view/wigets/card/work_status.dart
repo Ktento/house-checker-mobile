615830d9968f80a31975dc108dd89ea327496666
Ktento <kento07210525mo@outlook.jp>
2025-11-07 14:59:50 +0900

    カードの背景色を白に変更

--- lib/view/wigets/card/work_status.dart
+++ lib/view/wigets/card/work_status.dart
@@ -23,15 +23,12 @@ class _WorkStatusState extends State<WorkStatus> {
       width: 252,
       height: 316,
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 112, 112, 219),
+        color: CupertinoColors.systemBackground,
         borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(255, 48, 48, 48),
-            blurRadius: 10,
-            offset: Offset(0, 5),
-          ),
-        ],
+        border: Border.all(
+          color: CupertinoColors.black,
+          width: 1,
+        ),
       ),
       child: Padding(
         padding: const EdgeInsets.only(left: 12, top: 3),
@@ -43,13 +40,13 @@ class _WorkStatusState extends State<WorkStatus> {
               Text(
                 '判定士作業状況',
                 style: TextStyle(
-                  color: CupertinoColors.white,
+                  color: CupertinoColors.black,
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               Icon(CupertinoIcons.graph_square,
-                  color: CupertinoColors.white, size: 20),
+                  color: CupertinoColors.black, size: 20),
             ]),
             SizedBox(height: 10),
             Expanded(
@@ -70,7 +67,7 @@ class _WorkStatusState extends State<WorkStatus> {
                             Text(
                               inspector['name']!,
                               style: const TextStyle(
-                                color: CupertinoColors.white,
+                                color: CupertinoColors.black,
                                 fontSize: 15,
                                 fontWeight: FontWeight.bold,
                               ),
@@ -79,7 +76,7 @@ class _WorkStatusState extends State<WorkStatus> {
                             Text(
                               inspector['progress']!,
                               style: const TextStyle(
-                                color: CupertinoColors.white,
+                                color: CupertinoColors.black,
                                 fontSize: 15,
                                 fontWeight: FontWeight.w500,
                               ),
