edec6f165b867cf39ee9909d0441a367c4c62e0e
Ktento <kento07210525mo@outlook.jp>
2025-11-07 17:08:23 +0900

    現在位置を別変数で保持するように変更

--- lib/models/map_model.dart
+++ lib/models/map_model.dart
@@ -1,6 +1,7 @@
 import 'package:latlong2/latlong.dart';
 
 class MapModel {
+  LatLng currentLocation = LatLng(35.6586, 139.7454);
   // マーカーのリスト
   List<LatLng> markers = [
     LatLng(35.6586, 139.7454), // 初期マーカー東京タワー
