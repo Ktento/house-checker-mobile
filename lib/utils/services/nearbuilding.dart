cf715275b442e8b63779a1ed2a5783180813a5ee
Ktento <kento07210525mo@outlook.jp>
2025-11-07 17:08:43 +0900

    緯度経度から周辺の判定リストを返す関数を作成

---
+++ lib/utils/services/nearbuilding.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:latlong2/latlong.dart';
+
+Future<List<LatLng>> sendRecord(LatLng now) async {
+  try {
+    final url = Uri.https(
+        'script.google.com',
+        '/macros/s/AKfycbxm62dd1yIWL6WWFN4Hr5J1hzjME79WUFYle5AY91kfsfrlmNYQimCaCWdlXLV_ocb3/exec',
+        {
+          'mode': 'lat_lng_search',
+          'lat': now.latitude.toString(),
+          'lng': now.longitude.toString(),
+          'rang': '300',
+          'extractKeys': 'latitude,longitude'
+        });
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      print('送信成功: ${response.body}');
+      final jsonResponse = jsonDecode(response.body);
+
+      List<dynamic> data = jsonResponse['data'];
+      List<LatLng> points =
+          data.map((e) => LatLng(e['latitude'], e['longitude'])).toList();
+
+      return points;
+    } else {
+      throw Exception('送信失敗: ${response.statusCode} ${response.body}');
+    }
+  } catch (e) {
+    print("データ保存処理のエラー:{$e}");
+    return [];
+  }
+}
