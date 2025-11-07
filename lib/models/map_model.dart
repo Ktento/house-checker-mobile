3d0fc20f8c308c7f4406e18cd20b6eef92a31c10
Ktento <kento07210525mo@outlook.jp>
2025-11-07 18:18:47 +0900

    Markerモデルを追加

--- lib/models/map_model.dart
+++ lib/models/map_model.dart
@@ -3,7 +3,14 @@ import 'package:latlong2/latlong.dart';
 class MapModel {
   LatLng currentLocation = LatLng(35.6586, 139.7454);
   // マーカーのリスト
-  List<LatLng> markers = [
-    LatLng(35.6586, 139.7454), // 初期マーカー東京タワー
-  ];
+  List<LatLng> EndBuildingmarkers = [];
+  List<LatLng> RiskBuildingmarkers = [];
+  List<LatLng> WaitingBuildingmarkers = [];
+}
+
+class MarkerData {
+  final LatLng position;
+  final String overallscore;
+
+  MarkerData({required this.position, required this.overallscore});
 }
