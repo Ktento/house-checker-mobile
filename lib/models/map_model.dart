474bd496a691c685ec17ce00767c9472b32c30ae
Ktento <kento07210525mo@outlook.jp>
2025-11-10 11:27:32 +0900

    危険度評価のマーカーを保持するリストを追加

--- lib/models/map_model.dart
+++ lib/models/map_model.dart
@@ -3,9 +3,13 @@ import 'package:latlong2/latlong.dart';
 class MapModel {
   LatLng currentLocation = LatLng(35.6586, 139.7454);
   // マーカーのリスト
-  List<LatLng> EndBuildingmarkers = [];
-  List<LatLng> RiskBuildingmarkers = [];
-  List<LatLng> WaitingBuildingmarkers = [];
+  //危険度評価　赤・黄色・緑のリスト
+  List<LatLng> redBuildingmarkers = [];
+  List<LatLng> yellowBuildingmarkers = [];
+  List<LatLng> greenBuildingmarkers = [];
+
+  //危険度評価未完了のリスト
+  List<LatLng> waitingBuildingmarkers = [];
 }
 
 class MarkerData {
@@ -13,4 +17,13 @@ class MarkerData {
   final String overallscore;
 
   MarkerData({required this.position, required this.overallscore});
+  factory MarkerData.fromJson(Map<String, dynamic> json) {
+    return MarkerData(
+      position: LatLng(
+        (json['latitude'] as num).toDouble(),
+        (json['longitude'] as num).toDouble(),
+      ),
+      overallscore: json['overallscore'] ?? '',
+    );
+  }
 }
