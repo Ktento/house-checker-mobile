363d5c5f8892e737d057deca6c69a8f23ee938af
Ktento <kento07210525mo@outlook.jp>
2025-11-14 14:59:55 +0900

    locationmodelをfreezedを使用してリファクタリング

--- lib/models/location_model.dart
+++ lib/models/location_model.dart
@@ -1,7 +1,12 @@
 import 'package:latlong2/latlong.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class LocationModel {
-  //初期位置東京タワー
-  LatLng now = LatLng(35.6586, 139.7454);
-  double? heading; // 方位（0-360度、未取得時はnull）
+part 'location_model.freezed.dart';
+
+@freezed
+class LocationState with _$LocationState {
+  const factory LocationState({
+    required LatLng now,
+    double? heading,
+  }) = _LocationState;
 }
---
+++ lib/models/location_model.freezed.dart
@@ -0,0 +1,160 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'location_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$LocationState {
+  LatLng get now => throw _privateConstructorUsedError;
+  double? get heading => throw _privateConstructorUsedError;
+
+  /// Create a copy of LocationState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $LocationStateCopyWith<LocationState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LocationStateCopyWith<$Res> {
+  factory $LocationStateCopyWith(
+          LocationState value, $Res Function(LocationState) then) =
+      _$LocationStateCopyWithImpl<$Res, LocationState>;
+  @useResult
+  $Res call({LatLng now, double? heading});
+}
+
+/// @nodoc
+class _$LocationStateCopyWithImpl<$Res, $Val extends LocationState>
+    implements $LocationStateCopyWith<$Res> {
+  _$LocationStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of LocationState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? now = null,
+    Object? heading = freezed,
+  }) {
+    return _then(_value.copyWith(
+      now: null == now
+          ? _value.now
+          : now // ignore: cast_nullable_to_non_nullable
+              as LatLng,
+      heading: freezed == heading
+          ? _value.heading
+          : heading // ignore: cast_nullable_to_non_nullable
+              as double?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$LocationStateImplCopyWith<$Res>
+    implements $LocationStateCopyWith<$Res> {
+  factory _$$LocationStateImplCopyWith(
+          _$LocationStateImpl value, $Res Function(_$LocationStateImpl) then) =
+      __$$LocationStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({LatLng now, double? heading});
+}
+
+/// @nodoc
+class __$$LocationStateImplCopyWithImpl<$Res>
+    extends _$LocationStateCopyWithImpl<$Res, _$LocationStateImpl>
+    implements _$$LocationStateImplCopyWith<$Res> {
+  __$$LocationStateImplCopyWithImpl(
+      _$LocationStateImpl _value, $Res Function(_$LocationStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of LocationState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? now = null,
+    Object? heading = freezed,
+  }) {
+    return _then(_$LocationStateImpl(
+      now: null == now
+          ? _value.now
+          : now // ignore: cast_nullable_to_non_nullable
+              as LatLng,
+      heading: freezed == heading
+          ? _value.heading
+          : heading // ignore: cast_nullable_to_non_nullable
+              as double?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$LocationStateImpl implements _LocationState {
+  const _$LocationStateImpl({required this.now, this.heading});
+
+  @override
+  final LatLng now;
+  @override
+  final double? heading;
+
+  @override
+  String toString() {
+    return 'LocationState(now: $now, heading: $heading)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$LocationStateImpl &&
+            (identical(other.now, now) || other.now == now) &&
+            (identical(other.heading, heading) || other.heading == heading));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, now, heading);
+
+  /// Create a copy of LocationState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$LocationStateImplCopyWith<_$LocationStateImpl> get copyWith =>
+      __$$LocationStateImplCopyWithImpl<_$LocationStateImpl>(this, _$identity);
+}
+
+abstract class _LocationState implements LocationState {
+  const factory _LocationState(
+      {required final LatLng now, final double? heading}) = _$LocationStateImpl;
+
+  @override
+  LatLng get now;
+  @override
+  double? get heading;
+
+  /// Create a copy of LocationState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$LocationStateImplCopyWith<_$LocationStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
