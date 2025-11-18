c277f6102b79b77c6a93f58567f5d123186e2446
Ktento <kento07210525mo@outlook.jp>
2025-11-18 11:18:31 +0900

    コントローラーををviewに移動

--- lib/view/wigets/card/progress_rate.dart
+++ lib/view/wigets/card/progress_rate.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import '../../../controllers/percent_controller.dart';
+import '../../../models/percent_model.dart';
 
 class ProgressRate extends StatefulWidget {
   const ProgressRate({super.key});
@@ -129,5 +129,36 @@ class _ProgressRateState extends State<ProgressRate>
         ),
       ),
     );
+  }
+}
+
+class PercentController {
+  late AnimationController animationController;
+  late PercentModel model;
+
+  PercentController(TickerProvider vsync) {
+    animationController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: vsync,
+    );
+
+    final animation = Tween<double>(begin: 0, end: 100).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+    );
+
+    model = PercentModel(animation);
+  }
+
+  void start({double to = 100}) {
+    animationController.reset();
+    final animation = Tween<double>(begin: 0, end: to).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+    );
+    model = PercentModel(animation);
+    animationController.forward();
+  }
+
+  void dispose() {
+    animationController.dispose();
   }
 }
