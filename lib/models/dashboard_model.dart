069b5aa9cd95bf33a9df8e2f6a4cf624db4069c7
Ktento <kento07210525mo@outlook.jp>
2025-11-26 12:39:08 +0900

    dashboard_modelをfreezedで作成

---
+++ lib/models/dashboard_model.dart
@@ -0,0 +1,78 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'dashboard_model.freezed.dart';
+part 'dashboard_model.g.dart';
+
+@freezed
+class DashboardData with _$DashboardData {
+  const factory DashboardData({
+    @Default(0) int totalbuilding, // 総建物数
+    @Default(0) int checkcomplete, // 判定完了数
+    @Default(0) int dangerbuilding, // 危険建物数
+    @Default(0) int checkwaiting, // 判定待ち
+    @Default(0.0) double completionRatioTotal, // 全体進捗 %
+    @Default(0.0) double dangerRatioCompleted, // Red と Yellow の合計割合 %
+
+    // 職員別判定件数 (キー: 職員名/ID, 値: 件数)
+    required Map<String, int> workercount,
+
+    // 判定結果の件数 red yellow greenの件数
+    required CheckSituation checksituation,
+
+    // 判定結果の割合　
+    required CheckSituationRatio checksituationRatio,
+
+    // 日別判定件数 (キー: 日付, 値: 件数)
+    required Map<String, int> datecount,
+
+    // 地域別分析データ (キー: 県名, 値: RegionAnalysis モデル)
+    required Map<String, RegionAnalysis> regionanalysis,
+  }) = _DashboardData;
+
+  factory DashboardData.fromJson(Map<String, dynamic> json) =>
+      _$DashboardDataFromJson(json);
+}
+
+/// 判定結果の件数
+@freezed
+class CheckSituation with _$CheckSituation {
+  const factory CheckSituation({
+    @Default(0) int red,
+    @Default(0) int yellow,
+    @Default(0) int green,
+    // JSON側のキーが日本語のため、@JsonKeyでマッピング
+    @JsonKey(name: '値なし', defaultValue: 0) required int noValue,
+  }) = _CheckSituation;
+
+  factory CheckSituation.fromJson(Map<String, dynamic> json) =>
+      _$CheckSituationFromJson(json);
+}
+
+/// 判定結果の割合 %
+@freezed
+class CheckSituationRatio with _$CheckSituationRatio {
+  const factory CheckSituationRatio({
+    @Default(0.0) double red,
+    @Default(0.0) double yellow,
+    @Default(0.0) double green,
+    @Default(0.0) double noValue,
+  }) = _CheckSituationRatio;
+
+  factory CheckSituationRatio.fromJson(Map<String, dynamic> json) =>
+      _$CheckSituationRatioFromJson(json);
+}
+
+/// 地域別分析データ
+@freezed
+class RegionAnalysis with _$RegionAnalysis {
+  const factory RegionAnalysis({
+    @Default(0) int totalbuilding,
+    @Default(0) int checkcomplete,
+    @Default(0) int dangerbuilding,
+    @Default(0) int checkwaiting,
+  }) = _RegionAnalysis;
+
+  factory RegionAnalysis.fromJson(Map<String, dynamic> json) =>
+      _$RegionAnalysisFromJson(json);
+}
---
+++ lib/models/dashboard_model.g.dart
@@ -0,0 +1,96 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dashboard_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$DashboardDataImpl _$$DashboardDataImplFromJson(Map<String, dynamic> json) =>
+    _$DashboardDataImpl(
+      totalbuilding: (json['totalbuilding'] as num?)?.toInt() ?? 0,
+      checkcomplete: (json['checkcomplete'] as num?)?.toInt() ?? 0,
+      dangerbuilding: (json['dangerbuilding'] as num?)?.toInt() ?? 0,
+      checkwaiting: (json['checkwaiting'] as num?)?.toInt() ?? 0,
+      completionRatioTotal:
+          (json['completionRatioTotal'] as num?)?.toDouble() ?? 0.0,
+      dangerRatioCompleted:
+          (json['dangerRatioCompleted'] as num?)?.toDouble() ?? 0.0,
+      workercount: Map<String, int>.from(json['workercount'] as Map),
+      checksituation: CheckSituation.fromJson(
+          json['checksituation'] as Map<String, dynamic>),
+      checksituationRatio: CheckSituationRatio.fromJson(
+          json['checksituationRatio'] as Map<String, dynamic>),
+      datecount: Map<String, int>.from(json['datecount'] as Map),
+      regionanalysis: (json['regionanalysis'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, RegionAnalysis.fromJson(e as Map<String, dynamic>)),
+      ),
+    );
+
+Map<String, dynamic> _$$DashboardDataImplToJson(_$DashboardDataImpl instance) =>
+    <String, dynamic>{
+      'totalbuilding': instance.totalbuilding,
+      'checkcomplete': instance.checkcomplete,
+      'dangerbuilding': instance.dangerbuilding,
+      'checkwaiting': instance.checkwaiting,
+      'completionRatioTotal': instance.completionRatioTotal,
+      'dangerRatioCompleted': instance.dangerRatioCompleted,
+      'workercount': instance.workercount,
+      'checksituation': instance.checksituation,
+      'checksituationRatio': instance.checksituationRatio,
+      'datecount': instance.datecount,
+      'regionanalysis': instance.regionanalysis,
+    };
+
+_$CheckSituationImpl _$$CheckSituationImplFromJson(Map<String, dynamic> json) =>
+    _$CheckSituationImpl(
+      red: (json['red'] as num?)?.toInt() ?? 0,
+      yellow: (json['yellow'] as num?)?.toInt() ?? 0,
+      green: (json['green'] as num?)?.toInt() ?? 0,
+      noValue: (json['値なし'] as num?)?.toInt() ?? 0,
+    );
+
+Map<String, dynamic> _$$CheckSituationImplToJson(
+        _$CheckSituationImpl instance) =>
+    <String, dynamic>{
+      'red': instance.red,
+      'yellow': instance.yellow,
+      'green': instance.green,
+      '値なし': instance.noValue,
+    };
+
+_$CheckSituationRatioImpl _$$CheckSituationRatioImplFromJson(
+        Map<String, dynamic> json) =>
+    _$CheckSituationRatioImpl(
+      red: (json['red'] as num?)?.toDouble() ?? 0.0,
+      yellow: (json['yellow'] as num?)?.toDouble() ?? 0.0,
+      green: (json['green'] as num?)?.toDouble() ?? 0.0,
+      noValue: (json['noValue'] as num?)?.toDouble() ?? 0.0,
+    );
+
+Map<String, dynamic> _$$CheckSituationRatioImplToJson(
+        _$CheckSituationRatioImpl instance) =>
+    <String, dynamic>{
+      'red': instance.red,
+      'yellow': instance.yellow,
+      'green': instance.green,
+      'noValue': instance.noValue,
+    };
+
+_$RegionAnalysisImpl _$$RegionAnalysisImplFromJson(Map<String, dynamic> json) =>
+    _$RegionAnalysisImpl(
+      totalbuilding: (json['totalbuilding'] as num?)?.toInt() ?? 0,
+      checkcomplete: (json['checkcomplete'] as num?)?.toInt() ?? 0,
+      dangerbuilding: (json['dangerbuilding'] as num?)?.toInt() ?? 0,
+      checkwaiting: (json['checkwaiting'] as num?)?.toInt() ?? 0,
+    );
+
+Map<String, dynamic> _$$RegionAnalysisImplToJson(
+        _$RegionAnalysisImpl instance) =>
+    <String, dynamic>{
+      'totalbuilding': instance.totalbuilding,
+      'checkcomplete': instance.checkcomplete,
+      'dangerbuilding': instance.dangerbuilding,
+      'checkwaiting': instance.checkwaiting,
+    };
