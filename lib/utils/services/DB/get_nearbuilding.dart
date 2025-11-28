e8201f2d1ad44415993e1473728c04631ac47dbb
Ktento <kento07210525mo@outlook.jp>
2025-11-28 11:48:48 +0900

    環境変数からURLを取得するように変更

--- lib/utils/services/DB/get_nearbuilding.dart
+++ lib/utils/services/DB/get_nearbuilding.dart
@@ -2,23 +2,20 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:latlong2/latlong.dart';
 import '../../../models/map_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<List<MarkerData>> getMarkers(LatLng now) async {
   try {
-    final url = Uri.https(
-        'script.google.com',
-        '/macros/s/AKfycbxm62dd1yIWL6WWFN4Hr5J1hzjME79WUFYle5AY91kfsfrlmNYQimCaCWdlXLV_ocb3/exec',
-        {
-          'mode': 'lat_lng_search',
-          'lat': now.latitude.toString(),
-          'lng': now.longitude.toString(),
-          'range': '3000',
-          'extractKeys': 'latitude,longitude,overallscore'
-        });
+    final url = Uri.https('script.google.com', dotenv.env['httpgas']!, {
+      'mode': 'lat_lng_search',
+      'lat': now.latitude.toString(),
+      'lng': now.longitude.toString(),
+      'range': '3000',
+      'extractKeys': 'latitude,longitude,overallscore'
+    });
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print('送信成功: ${response.body}');
       final jsonResponse = jsonDecode(response.body);
 
       List<dynamic> data = jsonResponse['data'];
@@ -30,7 +27,7 @@ Future<List<MarkerData>> getMarkers(LatLng now) async {
       throw Exception('送信失敗: ${response.statusCode} ${response.body}');
     }
   } catch (e) {
-    print("データ保存処理のエラー:{$e}");
+    print("エラー:(getMarkers):{$e}");
     return [];
   }
 }
