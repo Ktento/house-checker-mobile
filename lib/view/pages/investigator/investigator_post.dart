36f864c9fd9977867c721eae132c8b83d75ca5b8
Ktento <kento07210525mo@outlook.jp>
2025-12-04 16:35:44 +0900

    コンクリート建築物用のボタンを追加

--- lib/view/pages/investigator/investigator_post.dart
+++ lib/view/pages/investigator/investigator_post.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import '../../../view_model/location_view_model.dart';
 import 'post/steelFrame/steelFrame_research_unit.dart';
+import 'post/rc/reabar_research_unit.dart';
 
 class InvestigatorPost extends StatelessWidget {
   const InvestigatorPost({super.key});
@@ -52,8 +53,22 @@ class InvestigatorPost extends StatelessWidget {
                 child: const Text('鉄筋建築物'),
               ),
               const SizedBox(height: 10),
-              CupertinoButton(
-                onPressed: null,
+              CupertinoButton.filled(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                      builder: (_) => MultiProvider(
+                        providers: [
+                          ChangeNotifierProvider.value(
+                            value: context.read<LocationViewModel>(),
+                          ),
+                        ],
+                        child: const RebarResearchUnit(),
+                      ),
+                    ),
+                  );
+                },
                 child: const Text('コンクリート建築物'),
               ),
             ],
