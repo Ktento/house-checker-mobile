041025aa1be104a5c2cbfc3401b2f7ec8f98fbcb
Ktento <kento07210525mo@outlook.jp>
2025-12-23 13:12:14 +0900

    一般人がする投稿と判定士がする投稿を区別するように変更

--- lib/utils/services/DB/send_record.dart
+++ lib/utils/services/DB/send_record.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void sendRecord(
+void inevestigatorSendRecord(
     {WoodenRecord? woodenRecord,
     SteelFrameRecord? steelFrameRecord,
     RebarRecord? rebarRecord}) async {
@@ -18,8 +18,9 @@ void sendRecord(
     return;
   }
   try {
-    final jsonBody = record.toJson();
-
+    final jsonBody = {
+      'investigator': record.toJson(),
+    };
     print('送信する record:');
     printLong(jsonBody);
 
@@ -29,7 +30,47 @@ void sendRecord(
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(record.toJson()),
+      body: jsonEncode(jsonBody),
+    );
+
+    if (response.statusCode != 200 || response.statusCode != 302) {
+      throw Exception('送信失敗: ${response.body}');
+    } else {
+      print("送信成功(sendRecord):");
+    }
+  } catch (e) {
+    print("エラー(sendRecord):{$e}");
+  }
+}
+
+void generalSendRecord(
+    {WoodenRecord? woodenRecord,
+    SteelFrameRecord? steelFrameRecord,
+    RebarRecord? rebarRecord}) async {
+  final record;
+  if (woodenRecord != null) {
+    record = woodenRecord;
+  } else if (steelFrameRecord != null) {
+    record = steelFrameRecord;
+  } else if (rebarRecord != null) {
+    record = rebarRecord;
+  } else {
+    return;
+  }
+  try {
+    final jsonBody = {
+      'general': record.toJson(),
+    };
+    print('送信する record:');
+    printLong(jsonBody);
+
+    final url = Uri.parse(
+      dotenv.env['gas']!,
+    );
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(jsonBody),
     );
 
     if (response.statusCode != 200 || response.statusCode != 302) {
