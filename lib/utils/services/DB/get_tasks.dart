7c7255b50105662f293f9ebb53b96fe7dc4a3499
Ktento <kento07210525mo@outlook.jp>
2025-12-24 12:16:43 +0900

    未判定リストを取得しTasks型の Listで返すgetTasksを作成

---
+++ lib/utils/services/DB/get_tasks.dart
@@ -0,0 +1,27 @@
+import 'package:house_check_mobile/models/dashboard_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<List<Tasks>> getTasks() async {
+  try {
+    final url = Uri.https('script.google.com', dotenv.env['httpgas']!, {
+      'mode': 'generalpost_list',
+    });
+    final response = await http.get(url);
+    print(response.body);
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+      
+      List<dynamic> data = jsonResponse['data'];
+      final List<Tasks> results = data.map((e) => Tasks.fromJson(e)).toList();
+
+      return results;
+    } else {
+      throw Exception('取得失敗: ${response.statusCode} ${response.body}');
+    }
+  } catch (e) {
+    print("エラー:(getTasks):{$e}");
+    return [];
+  }
+}
