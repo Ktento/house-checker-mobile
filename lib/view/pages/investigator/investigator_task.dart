06ba46d32bce5e66c692f27723f756f198045496
Ktento <kento07210525mo@outlook.jp>
2025-12-24 12:25:39 +0900

    タスクを表示する画面を作成

---
+++ lib/view/pages/investigator/investigator_task.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import '../../wigets/card/todotask.dart';
+import '../../../view_model/dashboard_view_model.dart';
+import 'package:provider/provider.dart';
+
+class InvestigatorTask extends StatelessWidget {
+  const InvestigatorTask({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<DashboardViewModel>();
+    return CupertinoPageScaffold(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TODOTasks(data: viewModel.tasks),
+        )),
+      ],
+    ));
+  }
+}
