f3dcd66d85caef626c68bb84ea48229cc4339f98
Ktento <kento07210525mo@outlook.jp>
2025-12-25 12:14:09 +0900

    完了済みのデータを取得できるように変更

--- lib/utils/services/DB/get_tasks.dart
+++ lib/utils/services/DB/get_tasks.dart
@@ -9,10 +9,10 @@ Future<List<Tasks>> getTasks() async {
       'mode': 'generalpost_list',
     });
     final response = await http.get(url);
-    print(response.body);
+    // print(response.body);
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
-      
+
       List<dynamic> data = jsonResponse['data'];
       final List<Tasks> results = data.map((e) => Tasks.fromJson(e)).toList();
 
@@ -22,6 +22,29 @@ Future<List<Tasks>> getTasks() async {
     }
   } catch (e) {
     print("エラー:(getTasks):{$e}");
+    return [];
+  }
+}
+
+Future<List<Tasks>> getCompletedTasks() async {
+  try {
+    final url = Uri.https('script.google.com', dotenv.env['httpgas']!, {
+      'mode': 'chekcomplete_list',
+    });
+    final response = await http.get(url);
+    print(response.body);
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+
+      List<dynamic> data = jsonResponse['data'];
+      final List<Tasks> results = data.map((e) => Tasks.fromJson(e)).toList();
+
+      return results;
+    } else {
+      throw Exception('取得失敗: ${response.statusCode} ${response.body}');
+    }
+  } catch (e) {
+    print("エラー:(getCompletedTasks):{$e}");
     return [];
   }
 }
