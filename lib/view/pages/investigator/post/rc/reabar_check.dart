ad306fc9f016414f0dc187f60c5c78af03597fa4
Ktento <kento07210525mo@outlook.jp>
2025-12-28 02:01:47 +0900

    widgetを分離し、tableで結果を表示するように変更

--- lib/view/pages/investigator/post/rc/reabar_check.dart
+++ lib/view/pages/investigator/post/rc/reabar_check.dart
@@ -2,18 +2,90 @@ import 'package:flutter/cupertino.dart';
 import 'package:house_check_mobile/view_model/investigator_post/rebar_view_model.dart';
 import '../../../../../models/investigator_model.dart';
 import 'package:provider/provider.dart';
-import '../../../../../utils/helpers/damageLevel.dart';
 import '../../../../../utils/services/DB/image_upload.dart';
 import '../../../../../utils/services/DB/send_record.dart';
+import '../../../../wigets/checkwiget.dart';
 
-class DangerSurveyFormPage extends StatelessWidget {
+class RebarDangerSurveyFormPage extends StatelessWidget {
   final String? uuid;
-  const DangerSurveyFormPage({super.key, this.uuid});
+  const RebarDangerSurveyFormPage({super.key, this.uuid});
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<RebarViewModel>();
     RebarRecord record = viewModel.rebarRecord!;
+
+    final List<Map<String, dynamic>> surveyItems1 = [
+      {
+        'title': '損傷度Ⅲ以上の損傷部材',
+        'record': record.content.hasSevereDamageMembers,
+        'options': ['A.無し', 'B.あり', ''],
+      },
+      {
+        'title': '隣接建築物・周辺地盤',
+        'record': record.content.adjacentBuildingRisk,
+        'options': ['A.危険無し', 'B.不明確', 'C.危険あり'],
+      },
+      {
+        'title': '地盤破壊による傾斜',
+        'record': record.content.groundFailureInclination,
+        'options': ['A.0.2m以下', 'B.0.2m～1.0m', 'C.1.0m超'],
+      },
+      {
+        'title': '不同沈下による傾斜',
+        'record': record.content.unevenSettlement,
+        'options': ['A.1/60以下', 'B.1/60～1/30', 'C.1/30超'],
+      },
+      {
+        'title': '柱の損傷度Ⅴ',
+        'record': record.content.percentColumnsDamageLevel5,
+        'options': ['A.1%以下', 'B.1%～10%', 'C.10%超'],
+      },
+      {
+        'title': '柱の損傷度Ⅳ',
+        'record': record.content.percentColumnsDamageLevel4,
+        'options': ['A.10%以下', 'B.10%～20%', 'C.20%超'],
+      },
+    ];
+    final List<Map<String, dynamic>> surveyItems2 = [
+      {
+        'title': '窓枠・ガラス',
+        'record': record.content.windowFrame,
+        'options': ['A.ほとんど無被害', 'B.歪み、ひび割れ', 'C.落下の危険有'],
+      },
+      {
+        'title': '外装材（モルタル・タイル・石貼り）',
+        'record': record.content.exteriorMaterialMortarTileStone,
+        'options': ['A.ほとんど無被害', 'B.部分的なひび割れ、隙間', 'C.顕著なひび割れ、剥離'],
+      },
+      {
+        'title': '外装材（ALC・PC・金属・ブロック）',
+        'record': record.content.exteriorMaterialALCPCMetalBlock,
+        'options': ['A.目地の亀裂程度', 'B.板に隙間がみられる', 'C.顕著な目地ずれ、板破損'],
+      },
+      {
+        'title': '看板・機器',
+        'record': record.content.signageAndEquipment,
+        'options': ['A.傾斜無し', 'B.わずかな傾斜', 'C.落下の危険有り'],
+      },
+      {
+        'title': '屋外階段',
+        'record': record.content.outdoorStairs,
+        'options': ['A.傾斜なし', 'B.わずかな傾斜', 'C.明瞭な傾斜'],
+      },
+      {
+        'title': 'その他',
+        'record': record.content.others,
+        'options': ['A.安全', 'B.要注意', 'C.危険'],
+      },
+    ];
+    final List<String> options = [
+      '1.建物全体又は一部の崩壊・落階',
+      '2.基礎の著しい破壊、上部構造との著しいずれ',
+      '3.建物全体又は一部の著しい傾斜',
+      '4.その他',
+    ];
+
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('応急危険度 判定調査表'),
@@ -24,139 +96,73 @@ class DangerSurveyFormPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildSection(context, '調査情報', [
-                _buildRow(context, '整理番号', record.unit.number),
-                _buildRow(
+              buildSection(context, '調査情報', [
+                buildRow(context, '整理番号', record.unit.number),
+                buildRow(
                     context, '調査回数 ', '${record.unit.surveyCount.toString()}回'),
-                _buildRow(
+                buildRow(
                     //調査人の一人目の氏名を表示
                     context,
                     '調査者氏名',
                     record.unit.investigator[0]),
-                _buildRow(context, '都道府県',
+                buildRow(context, '都道府県',
                     record.unit.investigatorPrefecture[0].toString()),
-                _buildRow(context, '調査人番号',
+                buildRow(context, '調査人番号',
                     record.unit.investigatorNumber[0].toString()),
-                _buildRow(context, '調査日時',
+                buildRow(context, '調査日時',
                     '${record.unit.date.year}/${record.unit.date.month}/${record.unit.date.day} '),
               ]),
-              _buildSection(context, '建築物概要', [
-                _buildRow(context, '名称', record.overview.buildingName),
-                _buildRow(context, '所在地', record.overview.address),
-                _buildRow(context, '用途', record.overview.buildingUse),
-                _buildRow(context, '構造形式', record.overview.structure),
-                _buildRow(context, '階数', record.overview.floors.toString()),
-                _buildRow(context, '建築物規模', record.overview.scale),
+              buildSection(context, '建築物概要', [
+                buildRow(context, '名称', record.overview.buildingName),
+                buildRow(context, '所在地', record.overview.address),
+                buildRow(context, '用途', record.overview.buildingUse),
+                buildRow(context, '構造形式', record.overview.structure),
+                buildRow(context, '階数', record.overview.floors.toString()),
+                buildRow(context, '建築物規模', record.overview.scale),
               ]),
-              _buildSection(context, '調査', [
-                Text('１.一見して危険と判断される',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(
-                  context,
-                  '内容',
-                  exteriorInspectionScoreToLabel(
-                      record.content.overallExteriorScore.toString()),
-                  labelWidth: 180,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('２.隣接建築物・周辺の地盤等及び構造躯体に関する危険度',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(
-                  context,
-                  '損傷度Ⅲ以上の損傷部材の有無',
-                  hasSevereDamageMembersToLabel(
-                      record.content.hasSevereDamageMembers?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '隣接建築物・周辺の地盤の破壊による危険度',
-                  adjacentBuildingRiskToLabel(
-                      record.content.adjacentBuildingRisk?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '地盤破壊による建築物全体の沈下',
-                  groundFailureInclinationToLabel(
-                      record.content.groundFailureInclination?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '不同沈下による建築物全体の傾斜',
-                  unevenSettlementToLabel(
-                      record.content.unevenSettlement?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '損傷度Ⅴの柱本数/調査柱本数',
-                  percentColumnsLevel5ToLabel(
-                      record.content.percentColumnsDamageLevel5?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '損傷度Ⅳの柱本数/調査柱本数',
-                  percentColumnsLevel4ToLabel(
-                      record.content.percentColumnsDamageLevel4?.name),
-                  labelWidth: 180,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('３.落下危険物・転倒危険物に関する危険度',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(context, '窓枠・窓ガラス',
-                    windowFrameToLabel(record.content.windowFrame?.name)),
-                _buildRow(
-                    context,
-                    '外装材(モルタル・タイル・石貼り等)',
-                    exteriorMaterialMortarTileStoneToLabel(
-                        record.content.exteriorMaterialMortarTileStone?.name)),
-                _buildRow(
-                    context,
-                    '外装材(ALC板・PC板・金属・ブロック等)',
-                    exteriorMaterialALCPCMetalBlockToLabel(
-                        record.content.exteriorMaterialALCPCMetalBlock?.name)),
-                _buildRow(
-                    context,
-                    '看板・機器類',
-                    signageAndEquipmentToLabel(
-                        record.content.signageAndEquipment?.name)),
-                _buildRow(context, '屋外階段',
-                    outdoorStairsToLabel(record.content.outdoorStairs?.name)),
-                _buildRow(
-                    context, 'その他', othersToLabel(record.content.others?.name)),
-              ]),
-              _buildSection(context, '危険度評価', [
-                _buildRow(context, '一見して危険と判断される',
-                    record.content.overallExteriorScore,
+              SizedBox(height: 8),
+              Text('１.一見して危険と判断される',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 8),
+              buildSurvey1Table(
+                  context, options, record.content.exteriorInspectionScore),
+              SizedBox(height: 8),
+              Text('２.隣接建築物・周辺の地盤等及び構造躯体に関する危険度',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(
+                height: 8,
+              ),
+              buildSurvey2Table(context, surveyItems1),
+              SizedBox(height: 8),
+              Text('３.落下危険物・転倒危険物に関する危険度',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 8),
+              buildSurvey2Table(context, surveyItems2),
+              buildSection(context, '危険度評価', [
+                buildRow(context, '一見して危険と判断される',
+                    record.content.overallExteriorScore?.name ?? "未入力",
                     labelWidth: 180),
-                _buildRow(context, '隣接建築物・周辺の地盤等及び構造躯体',
+                buildRow(context, '隣接建築物・周辺の地盤等及び構造躯体',
                     record.content.overallStructuralScore?.name ?? "未入力",
                     labelWidth: 180),
-                _buildRow(context, '落下危険物・転倒危険物に関する危険度',
+                buildRow(context, '落下危険物・転倒危険物に関する危険度',
                     record.content.overallFallingObjectScore?.name ?? "未入力",
                     labelWidth: 180),
               ]),
-              _buildSection(context, '総合判定', [
-                _buildRow(context, '判定', record.overallScore.name),
+              buildSection(context, '総合判定', [
+                buildRow(context, '判定', record.overallScore.name),
               ]),
-              // _buildSection('コメント', [
-              //   _buildRow('備考', comment),
+              // buildSection('コメント', [
+              //   buildRow('備考', comment),
               // ]),
               SizedBox(height: 20),
               Center(
@@ -184,54 +190,4 @@ class DangerSurveyFormPage extends StatelessWidget {
       ),
     );
   }
-}
-
-/// 表示用のセクション
-Widget _buildSection(
-    BuildContext context, String title, List<Widget> children) {
-  final separatorColor = CupertinoColors.separator.resolveFrom(context);
-
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-          color: CupertinoColors.activeBlue,
-        ),
-      ),
-      SizedBox(height: 8),
-      ...children,
-      Container(height: 2, color: separatorColor),
-    ],
-  );
-}
-
-/// 1行表示
-Widget _buildRow(BuildContext context, String label, String value,
-    {double labelWidth = 140}) {
-  final separatorColor =
-      CupertinoColors.separator.resolveFrom(context).withOpacity(0.3);
-  return Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Row(
-          children: [
-            SizedBox(
-                width: labelWidth,
-                child:
-                    Text(label, style: TextStyle(fontWeight: FontWeight.w500))),
-            Expanded(
-              child:
-                  Text(value, style: TextStyle(color: CupertinoColors.label)),
-            ),
-          ],
-        ),
-      ),
-      Container(height: 1, color: separatorColor),
-    ],
-  );
 }
--- lib/view/pages/investigator/post/steelFrame/steelFrame_check.dart
+++ lib/view/pages/investigator/post/steelFrame/steelFrame_check.dart
@@ -3,19 +3,108 @@ import 'package:house_check_mobile/view_model/investigator_post/steelFrame_view_
 import '../../../../../models/investigator_model.dart';
 import '../../../../../utils/services/DB/send_record.dart';
 import 'package:provider/provider.dart';
-import '../../../../../view_model/Form_view_model.dart';
-import '../../../../../utils/helpers/damageLevel.dart';
 import '../../../../../utils/services/DB/image_upload.dart';
+import '../../../../wigets/checkwiget.dart';
 
-class DangerSurveyFormPage extends StatelessWidget {
+class SteelFrameDangerSurveyFormPage extends StatelessWidget {
   final String? uuid;
-  const DangerSurveyFormPage({super.key, this.uuid});
+  const SteelFrameDangerSurveyFormPage({super.key, this.uuid});
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<SteelFrameViewModel>();
-    final inputVM = context.read<FormViewModel>();
     SteelFrameRecord record = viewModel.steelFrameRecord!;
+    final List<Map<String, dynamic>> surveyItems1 = [
+      {
+        'title': '隣接建築物・周辺地盤',
+        'record': record.content.adjacentBuildingRisk,
+        'options': ['A.危険無し', 'B.不明確', 'C.危険あり'],
+      },
+      {
+        'title': '不同沈下による傾斜',
+        'record': record.content.unevenSettlement,
+        'options': ['A.1/300以下', 'B.1/300～1/100', 'C.1/100超'],
+      },
+      {
+        'title': '上階数1以下の傾斜',
+        'record': record.content.upperFloorLe1,
+        'options': ['A.1/100以下', 'B.1/100～1/30', 'C.1/30超'],
+      },
+      {
+        'title': '上階数2以下の傾斜',
+        'record': record.content.upperFloorLe2,
+        'options': ['A.1/200以下', 'B.1/200～1/50', 'C.1/50超'],
+      },
+      {
+        'title': '部材の座屈',
+        'record': record.content.hasBuckling,
+        'options': ['A.無し', 'B.局部座屈あり', 'C.全体座屈または著しい局部座屈'],
+      },
+      {
+        'title': '筋違の破断率',
+        'record': record.content.bracingBreakRate,
+        'options': ['A.20%以下', 'B.20%～50%', 'C.50%超'],
+      },
+      {
+        'title': '柱梁接合部・継手',
+        'record': record.content.jointFailure,
+        'options': ['A.無し', 'B.一部破断・亀裂', 'C.20%以上の破断'],
+      },
+      {
+        'title': '柱脚の破損',
+        'record': record.content.columnBaseDamage,
+        'options': ['A.無し', 'B.部分的', 'C.著しい'],
+      },
+      {
+        'title': '腐食',
+        'record': record.content.corrosion,
+        'options': ['A.ほとんど無し', 'B.各所に著しい錆', 'C.孔食が各所に見られる'],
+      },
+    ];
+    final List<Map<String, dynamic>> surveyItems2 = [
+      {
+        'title': '屋根材',
+        'record': record.content.roofingMaterial,
+        'options': ['A.ほとんど無被害', 'B.著しいずれ', 'C.全面的にずれ、破損'],
+      },
+      {
+        'title': '窓枠・ガラス',
+        'record': record.content.windowFrame,
+        'options': ['A.ほとんど無被害', 'B.歪み、ひび割れ', 'C.落下の危険有'],
+      },
+      {
+        'title': '外装材（湿式）',
+        'record': record.content.exteriorWet,
+        'options': ['A.ほとんど無被害', 'B.部分的なひび割れ、隙間', 'C.顕著なひび割れ、剥離'],
+      },
+      {
+        'title': '外装材（乾式）',
+        'record': record.content.exteriorDry,
+        'options': ['A.目地の亀裂程度', 'B.板に隙間がみられる', 'C.顕著な目地ずれ、板破損'],
+      },
+      {
+        'title': '看板・機器',
+        'record': record.content.signageAndEquipment,
+        'options': ['A.傾斜無し', 'B.わずかな傾斜', 'C.落下の危険有り'],
+      },
+      {
+        'title': '屋外階段',
+        'record': record.content.outdoorStairs,
+        'options': ['A.傾斜なし', 'B.わずかな傾斜', 'C.明瞭な傾斜'],
+      },
+      {
+        'title': 'その他',
+        'record': record.content.others,
+        'options': ['A.安全', 'B.要注意', 'C.危険'],
+      },
+    ];
+    final List<String> options = [
+      '1.建物全体又は一部の崩壊・落階',
+      '2.基礎の著しい破壊、上部構造との著しいずれ',
+      '3.建物全体又は一部の著しい傾斜',
+      '4.その他',
+    ];
+
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('応急危険度 判定調査表'),
@@ -26,154 +115,73 @@ class DangerSurveyFormPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildSection(context, '調査情報', [
-                _buildRow(context, '整理番号', record.unit.number),
-                _buildRow(
+              buildSection(context, '調査情報', [
+                buildRow(context, '整理番号', record.unit.number),
+                buildRow(
                     context, '調査回数 ', '${record.unit.surveyCount.toString()}回'),
-                _buildRow(
+                buildRow(
                     //調査人の一人目の氏名を表示
                     context,
                     '調査者氏名',
                     record.unit.investigator[0]),
-                _buildRow(context, '都道府県',
+                buildRow(context, '都道府県',
                     record.unit.investigatorPrefecture[0].toString()),
-                _buildRow(context, '調査人番号',
+                buildRow(context, '調査人番号',
                     record.unit.investigatorNumber[0].toString()),
-                _buildRow(context, '調査日時',
+                buildRow(context, '調査日時',
                     '${record.unit.date.year}/${record.unit.date.month}/${record.unit.date.day} '),
               ]),
-              _buildSection(context, '建築物概要', [
-                _buildRow(context, '名称', record.overview.buildingName),
-                _buildRow(context, '所在地', record.overview.address),
-                _buildRow(context, '用途', record.overview.buildingUse),
-                _buildRow(context, '構造形式', record.overview.structure),
-                _buildRow(context, '階数', record.overview.floors.toString()),
-                _buildRow(context, '建築物規模', record.overview.scale),
+              buildSection(context, '建築物概要', [
+                buildRow(context, '名称', record.overview.buildingName),
+                buildRow(context, '所在地', record.overview.address),
+                buildRow(context, '用途', record.overview.buildingUse),
+                buildRow(context, '構造形式', record.overview.structure),
+                buildRow(context, '階数', record.overview.floors.toString()),
+                buildRow(context, '建築物規模', record.overview.scale),
               ]),
-              _buildSection(context, '調査', [
-                Text('１.一見して危険と判断される',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(
-                  context,
-                  '内容',
-                  exteriorInspectionScoreToLabel(
-                      record.content.overallExteriorScore.toString()),
-                  labelWidth: 180,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('２.隣接建築物・周辺の地盤等及び構造躯体に関する危険度',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(
-                  context,
-                  '隣接建築物・周辺の地盤の破壊による危険度',
-                  adjacentBuildingRiskToLabel(
-                      record.content.adjacentBuildingRisk?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '構造躯体の不同沈下による建築物全体の傾斜',
-                  unevenSettlementToLabel(
-                      record.content.unevenSettlement?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '傾斜を生じた階の上の階数が1階以下の場合',
-                  upperFloorLe1ToLabel(record.content.upperFloorLe1?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '傾斜を生じた階の上の階数が2階以下の場合',
-                  upperFloorLe2ToLabel(record.content.upperFloorLe2?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '部材の座屈の有無',
-                  hasBucklingToLabel(record.content.hasBuckling?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '筋違の破断率',
-                  bracingBreakRateToLabel(
-                      record.content.bracingBreakRate?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '柱梁接合部および継手の破壊',
-                  jointFailureToLabel(record.content.jointFailure?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '柱脚の破損',
-                  columnBaseDamageToLabel(
-                      record.content.columnBaseDamage?.name),
-                  labelWidth: 180,
-                ),
-                _buildRow(
-                  context,
-                  '腐食の有無',
-                  corrosionToLabel(record.content.corrosion?.name),
-                  labelWidth: 180,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('３.落下危険物・転倒危険物に関する危険度',
-                    style: TextStyle(
-                        inherit: false,
-                        fontSize: 17,
-                        color: const Color.fromARGB(255, 140, 140, 246))),
-                _buildRow(
-                    context,
-                    '屋根材',
-                    roofOrSignboardRiskToLabel(
-                        record.content.roofingMaterial?.name)),
-                _buildRow(context, '窓枠・窓ガラス',
-                    windowFrameToLabel(record.content.windowFrame?.name)),
-                _buildRow(context, '外装材（湿式）',
-                    exteriorWetToLabel(record.content.exteriorWet?.name)),
-                _buildRow(context, '外装材（乾式）',
-                    exteriorDryToLabel(record.content.exteriorDry?.name)),
-                _buildRow(
-                    context,
-                    '看板・機器類',
-                    signageAndEquipmentToLabel(
-                        record.content.signageAndEquipment?.name)),
-                _buildRow(context, '屋外階段',
-                    outdoorStairsToLabel(record.content.outdoorStairs?.name)),
-                _buildRow(
-                    context, 'その他', othersToLabel(record.content.others?.name)),
-              ]),
-              _buildSection(context, '危険度評価', [
-                _buildRow(context, '一見して危険と判断される',
-                    record.content.overallExteriorScore,
+              SizedBox(height: 8),
+              Text('１.一見して危険と判断される',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 8),
+              buildSurvey1Table(
+                  context, options, record.content.exteriorInspectionScore),
+              SizedBox(height: 8),
+              Text('２.隣接建築物・周辺の地盤等及び構造躯体に関する危険度',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(
+                height: 8,
+              ),
+              buildSurvey2Table(context, surveyItems1),
+              SizedBox(height: 8),
+              Text('３.落下危険物・転倒危険物に関する危険度',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: const Color.fromARGB(255, 0, 0, 0))),
+              SizedBox(height: 8),
+              buildSurvey2Table(context, surveyItems2),
+              buildSection(context, '危険度評価', [
+                buildRow(context, '一見して危険と判断される',
+                    record.content.overallExteriorScore?.name ?? "未入力",
                     labelWidth: 180),
-                _buildRow(context, '隣接建築物・周辺の地盤等及び構造躯体',
+                buildRow(context, '隣接建築物・周辺の地盤等及び構造躯体',
                     record.content.overallStructuralScore?.name ?? "未入力",
                     labelWidth: 180),
-                _buildRow(context, '落下危険物・転倒危険物に関する危険度',
+                buildRow(context, '落下危険物・転倒危険物に関する危険度',
                     record.content.overallFallingObjectScore?.name ?? "未入力",
                     labelWidth: 180),
               ]),
-              _buildSection(context, '総合判定', [
-                _buildRow(context, '判定', record.overallScore.name),
+              buildSection(context, '総合判定', [
+                buildRow(context, '判定', record.overallScore.name),
               ]),
-              // _buildSection('コメント', [
-              //   _buildRow('備考', comment),
+              // buildSection('コメント', [
+              //   buildRow('備考', comment),
               // ]),
               SizedBox(height: 20),
               Center(
@@ -202,54 +210,4 @@ class DangerSurveyFormPage extends StatelessWidget {
       ),
     );
   }
-}
-
-/// 表示用のセクション
-Widget _buildSection(
-    BuildContext context, String title, List<Widget> children) {
-  final separatorColor = CupertinoColors.separator.resolveFrom(context);
-
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 18,
-          color: CupertinoColors.activeBlue,
-        ),
-      ),
-      SizedBox(height: 8),
-      ...children,
-      Container(height: 2, color: separatorColor),
-    ],
-  );
-}
-
-/// 1行表示
-Widget _buildRow(BuildContext context, String label, String value,
-    {double labelWidth = 140}) {
-  final separatorColor =
-      CupertinoColors.separator.resolveFrom(context).withOpacity(0.3);
-  return Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Row(
-          children: [
-            SizedBox(
-                width: labelWidth,
-                child:
-                    Text(label, style: TextStyle(fontWeight: FontWeight.w500))),
-            Expanded(
-              child:
-                  Text(value, style: TextStyle(color: CupertinoColors.label)),
-            ),
-          ],
-        ),
-      ),
-      Container(height: 1, color: separatorColor),
-    ],
-  );
 }
