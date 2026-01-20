cc1414863c402980981b9f1870ebf4ab36a25003
Ktento <kento07210525mo@outlook.jp>
2026-01-20 16:04:00 +0900

    カメラを使用するボタンを追加

--- lib/view/pages/investigator/investigator_post.dart
+++ lib/view/pages/investigator/investigator_post.dart
@@ -4,9 +4,39 @@ import 'package:provider/provider.dart';
 import '../../../view_model/location_view_model.dart';
 import 'post/steelFrame/steelFrame_research_unit.dart';
 import 'post/rc/reabar_research_unit.dart';
+import '../../wigets/camera.dart';
+import 'package:camera/camera.dart';
 
-class InvestigatorPost extends StatelessWidget {
+class InvestigatorPost extends StatefulWidget {
   const InvestigatorPost({super.key});
+
+  @override
+  State<InvestigatorPost> createState() => _InvestigatorPostState();
+}
+
+class _InvestigatorPostState extends State<InvestigatorPost> {
+  CameraDescription? _firstCamera; // カメラを保持する変数
+
+  @override
+  void initState() {
+    super.initState();
+    _setupCamera(); // 初期化時にカメラを取得
+  }
+
+  // カメラを取得するメソッド
+  Future<void> _setupCamera() async {
+    try {
+      final cameras = await availableCameras();
+      if (cameras.isNotEmpty) {
+        setState(() {
+          _firstCamera = cameras.first; // 背面カメラ（通常は最初）を取得
+        });
+      }
+    } catch (e) {
+      print("カメラの取得に失敗しました: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -70,6 +100,18 @@ class InvestigatorPost extends StatelessWidget {
                   );
                 },
                 child: const Text('コンクリート建築物'),
+              ),
+              CupertinoButton.filled(
+                child: const Text('カメラを起動'),
+                onPressed: () {
+                  // カメラ画面へ遷移
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (context) =>
+                          CameraPreviewScreen(camera: _firstCamera!),
+                    ),
+                  );
+                },
               ),
             ],
           ),
